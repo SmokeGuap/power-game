5f85a9ed1a8920199f23eab49588738d050fc1f9
Kirill <mustang.horse34@gmail.com>
2024-03-01 10:55:11 +0700

    add animation to scale

--- src/components/ImageComponents/Scale/Scale.tsx
+++ src/components/ImageComponents/Scale/Scale.tsx
@@ -6,12 +6,23 @@ import ScaleLevels from 'src/assets/images/scale-1.png';
 import styles from './Scale.module.scss';
 
 const Scale: FC = () => {
+  const [scale, setScale] = useState(1);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setScale(Math.floor(Math.random() * 101));
+    }, 300);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <>
       <img src={ScaleImage} className={styles.scale} alt='Scale' />
       <img src={ScaleLevels} className={styles.scaleLevels} alt='ScaleLevels' />
       <div className={styles.power}>
         <div
+          style={{ height: scale + '%' }}
           className={styles.powerLevel}
         ></div>
       </div>
