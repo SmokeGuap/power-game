ad861fc6b6a19edb9f364a5cc74f5343496b7aa6
Kirill <mustang.horse34@gmail.com>
2024-03-01 11:13:32 +0700

    add Hammer logic

--- src/components/ImageComponents/Hammer/Hammer.tsx
+++ src/components/ImageComponents/Hammer/Hammer.tsx
@@ -1,11 +1,24 @@
-import { FC } from 'react';
+import classNames from 'classnames';
+import { FC, useContext } from 'react';
 
 import HammerImage from 'src/assets/images/hammer.png';
+import { StateContext } from 'src/context';
 
 import styles from './Hammer.module.scss';
 
 const Hammer: FC = () => {
-  return <img src={HammerImage} className={styles.hammer} alt='Hammer' />;
+  const { gameStart, punch } = useContext(StateContext);
+
+  return (
+    <img
+      src={HammerImage}
+      className={classNames(styles.hammer, {
+        [styles.hammerActive]: gameStart,
+        [styles.hammerPunch]: punch,
+      })}
+      alt='Hammer'
+    />
+  );
 };
 
 export default Hammer;
--- src/context/StateContext.tsx
+++ src/context/StateContext.tsx
@@ -4,6 +4,8 @@ import { createContext } from 'react';
 interface IStateContext {
   gameStart: boolean;
   setGameStart: Dispatch<SetStateAction<boolean>>;
+  punch: boolean;
+  setPunch: Dispatch<SetStateAction<boolean>>;
 }
 
 interface IStateProviderProps extends React.DOMAttributes<HTMLDivElement> {
@@ -13,16 +15,21 @@ interface IStateProviderProps extends React.DOMAttributes<HTMLDivElement> {
 const StateContext = createContext<IStateContext>({
   gameStart: false,
   setGameStart: () => {},
+  punch: false,
+  setPunch: () => {},
 });
 
 const StateProvider: FC<IStateProviderProps> = ({ children }) => {
   const [gameStart, setGameStart] = useState<boolean>(false);
+  const [punch, setPunch] = useState<boolean>(false);
 
   return (
     <StateContext.Provider
       value={{
         gameStart,
         setGameStart,
+        punch,
+        setPunch,
       }}
     >
       {children}
