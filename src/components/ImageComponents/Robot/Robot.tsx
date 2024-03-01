e3c9070d55e69c8380a5f01fcbefafa6f434b808
Kirill <mustang.horse34@gmail.com>
2024-03-01 11:41:04 +0700

    update Robot Component

--- src/components/ImageComponents/Robot/Robot.tsx
+++ src/components/ImageComponents/Robot/Robot.tsx
@@ -2,16 +2,19 @@ import { FC, useContext } from 'react';
 
 import RobotImage from 'src/assets/images/robot_1.png';
 import RobotImage2 from 'src/assets/images/robot_2.png';
+import RobotImage3 from 'src/assets/images/robot_3.png';
 import { StateContext } from 'src/context';
 
 import styles from './Robot.module.scss';
 
 const Robot: FC = () => {
-  const { punch } = useContext(StateContext);
+  const { punch, powerOfPunch } = useContext(StateContext);
 
   return (
     <>
-      {punch ? (
+      {punch && powerOfPunch > 80 ? (
+        <img src={RobotImage3} className={styles.robot} alt='Robot' />
+      ) : punch ? (
         <img src={RobotImage2} className={styles.robot} alt='Robot' />
       ) : (
         <img src={RobotImage} className={styles.robot} alt='Robot' />
