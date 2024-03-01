41745db8aa183c9ed91b922a74817d42f92135b8
Kirill <mustang.horse34@gmail.com>
2024-03-01 11:23:08 +0700

    update PunchButton

--- src/components/ImageComponents/PunchButton/PunchButton.tsx
+++ src/components/ImageComponents/PunchButton/PunchButton.tsx
@@ -1,11 +1,23 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
 
 import Button from 'src/assets/images/button.png';
+import ButtonActive from 'src/assets/images/button_active.png';
+import { StateContext } from 'src/context';
 
 import styles from './PunchButton.module.scss';
 
 const PunchButton: FC = () => {
-  return <img src={Button} className={styles.button} alt='PunchButton' />;
+  const { punch } = useContext(StateContext);
+
+  return (
+    <>
+      {punch ? (
+        <img src={ButtonActive} className={styles.button} alt='PunchButton' />
+      ) : (
+        <img src={Button} className={styles.button} alt='PunchButton' />
+      )}
+    </>
+  );
 };
 
 export default PunchButton;
